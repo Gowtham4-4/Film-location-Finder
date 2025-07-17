346c87af25aeed96aa7628e33207a23cd40e58fc
Gowtham4-4 <gowthamperumallapalli@gmail.com>
2025-07-17 21:07:39 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // config.js
-
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = 'http://localhost:5000/api';
+const IMAGE_BASE_URL = 'http://localhost:5000'; // <-- New
+export { API_BASE_URL, IMAGE_BASE_URL };
 
 
