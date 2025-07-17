bf69a85ee8862c4d358a36caf062debdb5b12683
Gowtham4-4 <gowthamperumallapalli@gmail.com>
2025-07-17 21:28:24 +0530

    Update AddLocationPage.js

--- src/pages/AddLocationPage.js
+++ src/pages/AddLocationPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import API_BASE_URL from '../config';
+export { API_BASE_URL, IMAGE_BASE_URL };
 
 
 const AddLocationPage = () => {
